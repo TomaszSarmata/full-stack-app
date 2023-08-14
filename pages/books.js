580b74d8f6b9f224f35cfa26f4ca906df82cac6c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-14 15:42:09 +0100

    added BooksGrid to the main BooksPage

--- pages/books.js
+++ pages/books.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import BooksGrid from "@/components/books/books-grid";
 
-export default function Books() {
+export default function BooksPage() {
   return (
-    <div>
-      <div className="bg-green-500">Books Page</div>
+    <div className="bg-green-500">
+      <BooksGrid></BooksGrid>
+      <div>Books Page</div>
       <Link href="/">Go to Home Page</Link>
     </div>
   );
