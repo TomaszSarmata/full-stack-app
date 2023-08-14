52cf8f352e8fba0c472af3e6209760f321d8b429
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-14 15:41:31 +0100

    created books.js with an array of books

---
+++ components/books/books-grid/books.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    title: "The Fellowship of the Ring",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    title: "Born a Crime",
+    author: "Trevor Noah",
+  },
+];
