a99c906034919b6e8ad552b9082d597f97f7949f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-14 11:05:54 +0100

    added a Link functionality to index.js and books.js pages

--- pages/books.js
+++ pages/books.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Books() {
-  return <div className="bg-green-500">Books Page</div>;
+  return (
+    <div>
+      <div className="bg-green-500">Books Page</div>
+      <Link href="/">Go to Home Page</Link>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-5xl bg-red-500">Hello</div>
-  )
+    <div>
+      <div className="text-5xl bg-red-500">Hello!</div>
+      <Link href="./books">Go to Books page</Link>
+    </div>
+  );
 }
