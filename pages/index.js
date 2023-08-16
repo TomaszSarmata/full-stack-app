b137684b4627cea0ce056852f4f53d9cd4fd147c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-16 14:00:02 +0100

    moved the programmatic button away to its own component and implemented the child component along with the footer and header

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Header from "@/components/shared/header";
+import Footer from "@/components/shared/footer";
+import ProgramaticButton from "@/components/shared/programmatic-button";
 
 export default function Home() {
-  const router = useRouter();
-  const handleClick = () => {
-    alert("You will now be navigated to the Books Page. Press OK to proceed");
-    router.push("./books");
-  };
   return (
-    <div>
-      <div className="text-5xl bg-red-500">Hello!</div>
-      <Link href="./books">Go to Books page</Link>
-      <div>
-        <button
-          onClick={handleClick}
-          className="bg-blue-500 text-white py-1 px-3 rounded-md"
-        >
-          Click me!
-        </button>
-      </div>
+    <div className="w-full">
+      <Header name="Home Page"></Header>
+
+      <ProgramaticButton pageName="Books" href="/books"></ProgramaticButton>
+
+      <Footer href="/books" pageName="Books"></Footer>
     </div>
   );
 }
