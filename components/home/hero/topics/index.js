462e1056d55109a0b2fad13c13e422c183ffd6ef
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-17 17:42:01 +0100

    added extra text

--- components/home/hero/topics/index.js
+++ components/home/hero/topics/index.js
@@ -11,7 +11,7 @@ const topics = [
 export default function Topics() {
   return (
     <div className="w-full flex flex-col gap-4">
-      <h3 className="text-2xl font-medium">Topics Covered</h3>
+      <h3 className="text-2xl font-medium">Topics covered in this course</h3>
       <ul className=" list-disc flex flex-col mb-4 space-y-2">
         {topics.map((topic, index) => {
           return <li key={index}>{topic}</li>;
