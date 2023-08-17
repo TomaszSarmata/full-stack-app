16566e875872f3a891238ba477b41ad500686d26
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-17 14:38:57 +0100

    implemented content component in both files

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import { useRouter } from "next/router";
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
 import ProgramaticButton from "@/components/shared/programmatic-button";
+import Content from "@/components/shared/content";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Header name="Home Page"></Header>
 
-      <ProgramaticButton pageName="Books" href="/books"></ProgramaticButton>
+      <Content>
+        <ProgramaticButton pageName="Books" href="/books"></ProgramaticButton>
+      </Content>
 
       <Footer href="/books" pageName="Books Page"></Footer>
     </div>
