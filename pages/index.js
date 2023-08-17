930d9352553c689afa6262f71ef5fbce0ac9ce81
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-17 15:49:36 +0100

    implemented hero component

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,18 @@ import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
 import ProgramaticButton from "@/components/shared/programmatic-button";
 import Content from "@/components/shared/content";
+import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Header name="Home Page"></Header>
+
+      <Hero
+        imgUrl="/images/profile.png"
+        title="Tomasz"
+        subtitle="I am a bootcamp student"
+      ></Hero>
 
       <Content>
         <ProgramaticButton pageName="Books" href="/books"></ProgramaticButton>
