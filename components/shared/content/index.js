84576575abed2263bcae9596d04c1b7a5e02e9d4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-17 15:09:37 +0100

    code clean-up

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,3 +1,3 @@
 export default function Content({ children }) {
-  return <div className="w-full max-w-6xl mx-auto">{children}</div>;
+  return <div className="w-full max-w-6xl mx-auto my-8">{children}</div>;
 }
