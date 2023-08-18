30d969e268422919e06b0c4bbe6abfbe1ac6c388
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-18 10:04:23 +0100

    added images paths to the books.js file in our api folder to display photos of each book in book item

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,6 +1,7 @@
 export default function BookItem({ book }) {
   return (
     <div className="bg-red-100">
+      <img src={book.imgUrl} alt="book image" />
       <p>{book.title}</p>
       <p>{book.author}</p>
     </div>
--- pages/api/books.js
+++ pages/api/books.js
@@ -3,14 +3,17 @@ export default function handler(req, res) {
     {
       title: "The Hobbit",
       author: "J.R.R. Tolkien",
+      imgUrl: "/images/1.png",
     },
     {
       title: "The Fellowship of the Ring",
       author: "J.R.R. Tolkien",
+      imgUrl: "/images/2.png",
     },
     {
       title: "Born a Crime",
       author: "Trevor Noah",
+      imgUrl: "/images/3.png",
     },
   ];
   res.json(books);
