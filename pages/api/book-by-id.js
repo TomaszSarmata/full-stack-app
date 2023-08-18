3505a13967ba3e83806f010fa7157fae508a15a9
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-18 12:24:42 +0100

    created an endpoint to return only a single book depending on the book.id

---
+++ pages/api/book-by-id.js
@@ -0,0 +1,31 @@
+export default function handler(req, res) {
+  const id = req.query.id;
+  let singleBook;
+  const books = [
+    {
+      id: "1",
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      imgUrl: "/images/1.png",
+    },
+    {
+      id: "2",
+      title: "The Fellowship of the Ring",
+      author: "J.R.R. Tolkien",
+      imgUrl: "/images/2.png",
+    },
+    {
+      id: "3",
+      title: "Born a Crime",
+      author: "Trevor Noah",
+      imgUrl: "/images/3.png",
+    },
+  ];
+  singleBook = books.find((book) => {
+    if (book.id === id) {
+      return true;
+    }
+    return false;
+  });
+  res.json({ book: singleBook });
+}
