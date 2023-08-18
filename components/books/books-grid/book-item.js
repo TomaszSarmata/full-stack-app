cfa242bd5db14c1e100a13b43ebeb6e1c3f47823
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-18 10:07:26 +0100

    added styling to the book-item

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,9 +1,9 @@
 export default function BookItem({ book }) {
   return (
-    <div className="bg-red-100">
-      <img src={book.imgUrl} alt="book image" />
-      <p>{book.title}</p>
-      <p>{book.author}</p>
+    <div className="border rounded-lg p-4">
+      <img className="rounded-lg" src={book.imgUrl} alt="book image" />
+      <p className="text-xl font-medium">{book.title}</p>
+      <p className="text-gray-700">{book.author}</p>
     </div>
   );
 }
