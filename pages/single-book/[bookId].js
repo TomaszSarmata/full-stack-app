95956cfd0b17ea9db8eeb8e7acf32a34b5d99649
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-19 08:26:03 +0100

    created a dynamic product page that will fetch details of a single book and display its content to the user

--- pages/single-book/[bookId].js
+++ pages/single-book/[bookId].js
@@ -1,9 +1,34 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function SingleBook() {
+  const [singleBook, setSingleBook] = useState(null);
   const router = useRouter();
   const { bookId } = router.query;
 
-  console.log("router", router);
-  return <div>{bookId}</div>;
+  useEffect(() => {
+    if (!bookId) return;
+    getSingleBook(bookId);
+  }, [bookId]);
+
+  const getSingleBook = async (id) => {
+    const res = await fetch(`/api/book-by-id?id=${id}`);
+    const data = await res.json();
+    const { book } = data;
+    setSingleBook(book);
+  };
+
+  console.log(singleBook);
+
+  if (!singleBook) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <img className="h-44" src={singleBook.imgUrl} alt="" />
+      <p>{singleBook.title}</p>
+      <p>{singleBook.author}</p>
+    </div>
+  );
 }
