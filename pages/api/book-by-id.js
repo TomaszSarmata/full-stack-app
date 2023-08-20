873e141e30c86072fcfdf11fc40f9828f8375e23
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-20 17:10:32 +0100

    code clean-up

--- pages/api/book-by-id.js
+++ pages/api/book-by-id.js
@@ -7,13 +7,14 @@ export default function handler(req, res) {
       title: "The Hobbit",
       author: "J.R.R. Tolkien",
       imgUrl: "/images/1.png",
-      linkToPurchase: "https://amzn.eu/d/3p7Fo8Q",
+      linkToPurchase: "https://amzn.eu/d/e7GXwuT",
     },
     {
       id: "2",
       title: "The Fellowship of the Ring",
       author: "J.R.R. Tolkien",
       imgUrl: "/images/2.png",
+
       linkToPurchase: "https://amzn.eu/d/3p7Fo8Q",
     },
     {
