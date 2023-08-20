97fb76e5b464bfefc00482c06e3f96f3f7b77c57
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-20 16:57:28 +0100

    added linkToPuchase property to each book in our api list that we will use to create a button to purchase for the user

--- pages/api/book-by-id.js
+++ pages/api/book-by-id.js
@@ -7,18 +7,21 @@ export default function handler(req, res) {
       title: "The Hobbit",
       author: "J.R.R. Tolkien",
       imgUrl: "/images/1.png",
+      linkToPurchase: "https://amzn.eu/d/3p7Fo8Q",
     },
     {
       id: "2",
       title: "The Fellowship of the Ring",
       author: "J.R.R. Tolkien",
       imgUrl: "/images/2.png",
+      linkToPurchase: "https://amzn.eu/d/3p7Fo8Q",
     },
     {
       id: "3",
       title: "Born a Crime",
       author: "Trevor Noah",
       imgUrl: "/images/3.png",
+      linkToPurchase: "https://amzn.eu/d/eSaDawR",
     },
   ];
   singleBook = books.find((book) => {
