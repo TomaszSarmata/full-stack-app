1d2cbadf7bbad66874bd2c3fda30c315f75c5939
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-21 12:19:43 +0100

    used third party library copy to clipboard to offer user a possibility of coping the link to purchase a book

--- pages/single-book/[bookId].js
+++ pages/single-book/[bookId].js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Hero from "@/components/home/hero";
 import Footer from "@/components/shared/footer";
 import Content from "@/components/shared/content";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function SingleBook() {
   const [singleBook, setSingleBook] = useState(null);
@@ -37,11 +38,22 @@ export default function SingleBook() {
         subtitle={singleBook.author}
       ></Hero>
       <Content>
-        <div className="w-full">
-          <div className="bg-blue-500 w-1/6 py-1 px-3 rounded-md text-white ">
+        <div className="w-full flex flex-col">
+          <div className=" w-1/6 bg-blue-500 hover:bg-blue-700 py-1 px-3 rounded-md text-white text-center">
             <a href={singleBook.linkToPurchase} target="_blank">
               Buy on Amazon
             </a>
+          </div>
+
+          <div>
+            <CopyToClipboard text={singleBook.linkToPurchase}>
+              <button
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 active:bg-red-100 w-1/6 mt-2 py-1 px-3 rounded-md text-white"
+              >
+                Copy link to Amazon
+              </button>
+            </CopyToClipboard>
           </div>
         </div>
       </Content>
