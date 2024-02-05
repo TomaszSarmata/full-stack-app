9d839cae4cedfb9b96fd629f15d1721c0339563b
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-05 14:36:06 +0000

    uploaded the books json file to the supabase and replaced the hardcoded array of books with the sql command to fetch data from our db

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,25 +1,29 @@
 import sql from "@/utils/postgres";
 
-export default function handler(req, res) {
-  const books = [
-    {
-      id: "1",
-      title: "The Hobbit",
-      author: "J.R.R. Tolkien",
-      imgUrl: "/images/1.png",
-    },
-    {
-      id: "2",
-      title: "The Fellowship of the Ring",
-      author: "J.R.R. Tolkien",
-      imgUrl: "/images/2.png",
-    },
-    {
-      id: "3",
-      title: "Born a Crime",
-      author: "Trevor Noah",
-      imgUrl: "/images/3.png",
-    },
-  ];
+export default async function handler(req, res) {
+  const books = await sql`
+    select * from books
+  `;
+
+  // const books = [
+  //   {
+  //     id: "1",
+  //     title: "The Hobbit",
+  //     author: "J.R.R. Tolkien",
+  //     imgUrl: "/images/1.png",
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "The Fellowship of the Ring",
+  //     author: "J.R.R. Tolkien",
+  //     imgUrl: "/images/2.png",
+  //   },
+  //   {
+  //     id: "3",
+  //     title: "Born a Crime",
+  //     author: "Trevor Noah",
+  //     imgUrl: "/images/3.png",
+  //   },
+  // ];
   res.json(books);
 }
