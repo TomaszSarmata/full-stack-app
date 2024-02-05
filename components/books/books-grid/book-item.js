861e06ae49f22d796a62978b4f3c198ce8a10e53
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-05 15:05:20 +0000

    updated the src from books images to reflect changes in json file that was uploaded to supabase

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -30,7 +30,7 @@ export default function BookItem({ book }) {
 
   return (
     <div className="border rounded-lg p-4 text-center">
-      <img className="rounded-lg" src={book.imgUrl} alt="book image" />
+      <img className="rounded-lg" src={book.img_url} alt="book image" />
       <p className="text-xl font-medium">{book.title}</p>
       <p className="text-gray-700">{book.author}</p>
       <div className="flex flex-row space-x-3 justify-center">
