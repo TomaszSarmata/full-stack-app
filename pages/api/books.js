f6272b299101bbedd5b4e93cb08a9c78f4c8ca64
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-05 15:04:28 +0000

    updated the books api endpoint to replace hardcoded data with the sql request

--- pages/api/books.js
+++ pages/api/books.js
@@ -5,25 +5,5 @@ export default async function handler(req, res) {
     select * from books
   `;
 
-  // const books = [
-  //   {
-  //     id: "1",
-  //     title: "The Hobbit",
-  //     author: "J.R.R. Tolkien",
-  //     imgUrl: "/images/1.png",
-  //   },
-  //   {
-  //     id: "2",
-  //     title: "The Fellowship of the Ring",
-  //     author: "J.R.R. Tolkien",
-  //     imgUrl: "/images/2.png",
-  //   },
-  //   {
-  //     id: "3",
-  //     title: "Born a Crime",
-  //     author: "Trevor Noah",
-  //     imgUrl: "/images/3.png",
-  //   },
-  // ];
   res.json(books);
 }
