53d1329b5af20b7e1b082152f15629ea271ed5bb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-07 10:03:42 +0000

    implemented the initial structure of the page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,17 @@
+import Header from "@/components/shared/header";
+import Content from "@/components/shared/content";
+import Footer from "@/components/shared/footer";
+
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <Header name="Contact"></Header>
+
+      <Content>
+        <div>Content</div>
+      </Content>
+
+      <Footer pageName="Home" href="/"></Footer>
+    </div>
+  );
 }
