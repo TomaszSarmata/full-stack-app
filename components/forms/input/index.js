b45308f5cc12103e76d68855aec5555a1ba626c8
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-07 15:46:25 +0000

    abstracted away handleChange to the child component and passed onChange the value as an  argument

--- components/forms/input/index.js
+++ components/forms/input/index.js
@@ -1,10 +1,14 @@
 export default function Input({ placeholder, value, onChange }) {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    onChange(value);
+  };
   return (
     <input
       placeholder={placeholder}
       className="bg-gray-200 w-96 py-3 px-6 rounded"
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
     />
   );
 }
