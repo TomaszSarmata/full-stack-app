51cb986a90f07d867fe8d878ddbd99d22c9d17db
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-07 14:10:19 +0000

    added extra check to each point to stop empty strings being sent out

--- pages/api/messages.js
+++ pages/api/messages.js
@@ -5,14 +5,17 @@ export default async function handler(req, res) {
   const email = req.query.email;
   const message = req.query.message;
 
-  if (name === null || name === undefined) {
+  if (name === null || name === undefined || name === "") {
     res.status(400).json({ message: "Plese provide the name" });
+    return;
   }
-  if (email === null || email === undefined) {
+  if (email === null || email === undefined || email === "") {
     res.status(400).json({ message: "Plese provide the email" });
+    return;
   }
-  if (message === null || message === undefined) {
+  if (message === null || message === undefined || message === "") {
     res.status(400).json({ message: "Plese provide the message" });
+    return;
   }
 
   const messages = await sql`
