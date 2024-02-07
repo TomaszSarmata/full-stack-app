5a61a5966eb3790c41e8b95d50f95e595051fbbd
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-07 11:40:37 +0000

    created an endpoint to update contact_messages on our db

---
+++ pages/api/messages.js
@@ -0,0 +1,25 @@
+import sql from "@/utils/postgres";
+
+export default async function handler(req, res) {
+  const name = req.query.name;
+  const email = req.query.email;
+  const message = req.query.message;
+
+  if (name === null || name === undefined) {
+    res.status(400).json({ message: "Plese provide the name" });
+  }
+  if (email === null || email === undefined) {
+    res.status(400).json({ message: "Plese provide the email" });
+  }
+  if (message === null || message === undefined) {
+    res.status(400).json({ message: "Plese provide the message" });
+  }
+
+  const messages = await sql`
+  insert into contact_messages 
+    (name, email, message)
+  values (${name},${email},${message})
+  `;
+
+  res.json({ message: "values updated successfully" });
+}
