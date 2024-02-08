5d5a0b2dfd4f0c24380749650de4d8ac176616f9
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-08 16:24:50 +0000

    added loading state to locations grid

--- components/locations/locations-grid/index.js
+++ components/locations/locations-grid/index.js
@@ -4,24 +4,33 @@ import LikesProvider from "@/context/likes";
 
 export default function LocationsGrid() {
   const [locations, setLocations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getLocations();
   }, []);
 
   const getLocations = async () => {
+    setIsLoading(true);
     const res = await fetch(`/api/locations`);
     const data = await res.json();
     setLocations(data);
+    setIsLoading(false);
   };
 
   return (
     <LikesProvider>
-      <div className="w-full grid grid-cols-3 gap-4 mb-8">
-        {locations.map((location, index) => {
-          return <LocationItem location={location} key={index}></LocationItem>;
-        })}
-      </div>
+      {isLoading ? (
+        <div>Loading locations...</div>
+      ) : (
+        <div className="w-full grid grid-cols-3 gap-4 mb-8">
+          {locations.map((location, index) => {
+            return (
+              <LocationItem location={location} key={index}></LocationItem>
+            );
+          })}
+        </div>
+      )}
     </LikesProvider>
   );
 }
