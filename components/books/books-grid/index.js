29b9104f3b7412ac041e68df39fd76842e56600b
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-08 16:30:23 +0000

    added loading state to books  grid

--- components/books/books-grid/index.js
+++ components/books/books-grid/index.js
@@ -3,22 +3,31 @@ import BookItem from "./book-item";
 
 export default function BooksGrid() {
   const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getBooks();
   }, []);
 
   const getBooks = async () => {
+    setIsLoading(true);
     const res = await fetch(`/api/books`);
     const data = await res.json();
     setBooks(data);
+    setIsLoading(false);
   };
 
   return (
-    <div className="w-full grid grid-cols-3 gap-4 mb-8">
-      {books.map((book, index) => {
-        return <BookItem book={book} key={index}></BookItem>;
-      })}
+    <div>
+      {isLoading ? (
+        <div>Loading books...</div>
+      ) : (
+        <div className="w-full grid grid-cols-3 gap-4 mb-8">
+          {books.map((book, index) => {
+            return <BookItem book={book} key={index}></BookItem>;
+          })}
+        </div>
+      )}
     </div>
   );
 }
