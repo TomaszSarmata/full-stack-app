ac85570baeb3b8a33dd4e2880c2e1c80e45396c6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-08 11:14:19 +0000

    created context and passed it our initial state

--- context/likes/index.js
+++ context/likes/index.js
@@ -1,4 +1,9 @@
+import { createContext } from "react";
+
 const INITIAL_STATE = {
+  //declared inital state
   likes: 0,
   handleLike: () => {},
 };
+
+const LikesContext = createContext(INITIAL_STATE); //created a context and passed it initial state
