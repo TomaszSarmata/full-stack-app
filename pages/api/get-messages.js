7e97d52632a4637fc894162cb3d0c136ed4c698c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-08 15:21:37 +0000

    added order to the displayed messages

--- pages/api/get-messages.js
+++ pages/api/get-messages.js
@@ -3,6 +3,7 @@ import sql from "@/utils/postgres";
 export default async function handler(req, res) {
   const messages = await sql`
     select * from contact_messages
+    order by created_at desc
   `;
   res.json(messages);
 }
