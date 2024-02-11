eb02136bf93e1652b57de873b2ffd8de51f9c6a2
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-11 12:04:20 +0000

    implemented the loading state and took the messages from the dependency array

--- components/contact/list-of-messages/index.js
+++ components/contact/list-of-messages/index.js
@@ -1,13 +1,14 @@
+import LoadingMessages from "@/components/shared/loading/messages";
 import { useEffect } from "react";
 import { useState } from "react";
 
 export default function ListOfMessages() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     getMessages();
-  }, [messages]);
+  }, []);
 
   const getMessages = async () => {
     setIsLoading(true);
@@ -17,25 +18,30 @@ export default function ListOfMessages() {
     setMessages(messages);
     setIsLoading(false);
   };
-  return (
+
+  return isLoading ? (
+    <div className="grid grid-cols-3 px-10 gap-4">
+      <LoadingMessages></LoadingMessages>
+      <LoadingMessages></LoadingMessages>
+      <LoadingMessages></LoadingMessages>
+    </div>
+  ) : (
     <div className="grid grid-cols-3  gap-6 w-4/5 px-2 mx-auto">
-      {messages.map((message, index) => {
-        return (
-          <div key={index} className="bg-blue-100 py-4 px-8 rounded-lg">
-            <p className="font-medium text-xl border-b-2 border-gray-400 border-opacity-40">
-              {message.name}
-            </p>
-            <p>
-              {message.email ? (
-                message.email
-              ) : (
-                <span className="italic text-gray-500">No Email Provided</span>
-              )}
-            </p>
-            <p>{message.message}</p>
-          </div>
-        );
-      })}
+      {messages.map((message, index) => (
+        <div key={index} className="bg-blue-100 py-4 px-8 rounded-lg">
+          <p className="font-medium text-xl border-b-2 border-gray-400 border-opacity-40">
+            {message.name}
+          </p>
+          <p>
+            {message.email ? (
+              message.email
+            ) : (
+              <span className="italic text-gray-500">No Email Provided</span>
+            )}
+          </p>
+          <p>{message.message}</p>
+        </div>
+      ))}
     </div>
   );
 }
