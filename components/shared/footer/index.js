11859727c33eb7ee2120241355bcd43c7752fc43
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-11 10:05:33 +0000

    added some temporary styling to the footer

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer({ pageName, href }) {
   return (
-    <div className="w-full max-w-6xl mx-auto mt-8">
+    <div className="w-full max-w-6xl mx-auto mt-8 block bg-red-100">
       <Link href={href}>Go to {pageName}</Link>
     </div>
   );
