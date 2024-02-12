0568a64ccdfff856f44cbadfd95f0c339d0ca557
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-12 12:13:55 +0000

    mobile optimised the home page for mobiles

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -1,12 +1,15 @@
 export default function Hero({ imgUrl, title, subtitle }) {
   return (
     <div className="w-full max-w-6xl mx-auto">
-      <div className="w-full flex flex-row space-x-3 mt-8">
-        <div className="w-2/12">
+      <div className="w-full flex flex-col  items-center py-4 space-y-4 md:flex-row md:space-x-4">
+        <div className="w-48 shadow-md rounded-lg">
+          {" "}
           <img className="rounded-lg " src={imgUrl} alt="" />
         </div>
-        <div className="w-10/12">
-          <h1 className="text-4xl font-bold">{title}</h1>
+        <div className="w-full text-center md:text-start">
+          <h1 className="text-6xl text-gray-700 sm:text-gray-500 md:text-purple-900 font-bold">
+            {title}
+          </h1>
           <p className="text-xl text-gray-700">{subtitle}</p>
         </div>
       </div>
--- components/home/hero/topics/index.js
+++ components/home/hero/topics/index.js
@@ -4,7 +4,7 @@ export default function Topics() {
   return (
     <div className="w-full flex flex-col gap-4">
       <h3 className="text-2xl font-medium">Topics covered in this course</h3>
-      <ul className=" list-disc flex flex-col mb-4 space-y-2">
+      <ul className=" list-disc flex flex-col mb-4 space-y-2 ml-4">
         {topics.map((topic, index) => {
           return <li key={index}>{topic}</li>;
         })}
