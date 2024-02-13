9c78e8e74a252042d161929a4669b5dc18a3f458
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-13 16:10:11 +0000

    added horizontal padding to the content component

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,3 +1,3 @@
 export default function Content({ children }) {
-  return <div className="w-full max-w-6xl mx-auto my-8">{children}</div>;
+  return <div className="w-full max-w-6xl mx-auto my-8 px-4">{children}</div>;
 }
