ddf0d5726c1004c03f094c18c9f9d68dd2835a90
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-13 16:22:38 +0000

    implemented some initial content for the search page to display results in a grid, also added some styling

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,30 @@
 import Header from "@/components/shared/header";
 import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
+import Input from "@/components/forms/input";
 export default function Search() {
   return (
     <div className="w-full">
       <Header name="Search Page"></Header>
-      <Content>Content</Content>
+      <Content>
+        <div className="w-full sm:w-8/12 md:w-6/12 lg:w-4/12 flex flex-row space-x-2">
+          <Input></Input>
+          <button
+            className="bg-blue-500 text-white px-6 py-2 rounded"
+            type="button"
+          >
+            S
+          </button>
+        </div>
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+          <div className="w-full h-40 bg-red-500"></div>
+          <div className="w-full h-40 bg-red-500"></div>
+          <div className="w-full h-40 bg-red-500"></div>
+          <div className="w-full h-40 bg-red-500"></div>
+          <div className="w-full h-40 bg-red-500"></div>
+          <div className="w-full h-40 bg-red-500"></div>
+        </div>
+      </Content>
       <Footer pageName="Home" href="/"></Footer>
     </div>
   );
