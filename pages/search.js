ed7fb75f667085cf6ddb391c320e8a57ed01bc2a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-14 10:17:12 +0000

    added extra media query class to make the font-size smaller

--- pages/search.js
+++ pages/search.js
@@ -36,7 +36,7 @@ export default function Search() {
                 alt="location"
                 className="brightness-50 rounded-md shadow-md"
               />
-              <p className="w-full text-3xl bold text-shadow absolute top-1/2 left-1/2 translate-x-[-50%] ">
+              <p className="w-full text-3xl bold text-shadow small-screen-text absolute top-1/2 left-1/2 translate-x-[-50%] ">
                 {location.title}
               </p>
               <p className="w-full text-xl bold text-shadow absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[120%]">
