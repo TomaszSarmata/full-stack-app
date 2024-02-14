52b8aeeb06cd8ba5224c214594b896b9176d349e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-14 13:48:53 +0000

    changed the sql command so that is not case sensitive (ilike - case insensitive)

--- pages/api/search-location.js
+++ pages/api/search-location.js
@@ -1,14 +1,17 @@
 import sql from "@/utils/postgres";
 
 export default async function handler(req, res) {
-  const input = req.query.inputValue;
+  // input = input[0].toUpperCase + input[1:]
+  //   let input = "san francisco";
+  // let formattedInput = input.split(' ').map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+  let input = req.query.inputValue;
   console.log(input, "input here");
 
   try {
     const pattern = input + "%";
     const location = await sql`
     select * from locations
-    where title like ${pattern}
+    where title ilike ${pattern}
     `;
     res.status(200).json(location);
   } catch (error) {
