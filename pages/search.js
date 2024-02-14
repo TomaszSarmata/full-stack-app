789c28d83a7507cd49decce0a10259ec86f99533
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-14 10:07:03 +0000

    added some styling

--- pages/search.js
+++ pages/search.js
@@ -34,7 +34,7 @@ export default function Search() {
               <img
                 src={location.img_url}
                 alt="location"
-                className="brightness-50"
+                className="brightness-50 rounded-md shadow-md"
               />
               <p className="w-full text-3xl bold text-shadow absolute top-1/2 left-1/2 translate-x-[-50%] ">
                 {location.title}
