a4b3aff74e98ceb8fa95ab7e89712e949c3a2426
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-16 17:29:17 +0000

    commented out pg library along with its connection file as it was causing the ssl certificate issues

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -1,5 +1,5 @@
 import sql from "@/utils/postgres";
-import pg from "@/utils/pg";
+// import pg from "@/utils/pg";
 
 export default async function handler(req, res) {
   let locations;
@@ -16,25 +16,26 @@ export default async function handler(req, res) {
       res.status(500).json({ error: "Internal Server Error" });
     }
   } else {
-    try {
-      locations = await pg.execute(`
-        select * from locations where title like '%${input}%'
-      `);
-    } catch (error) {
-      console.error("Failed to fetch the location/s:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-
     // try {
-    //   const pattern = "%" + input + "%";
-    //   locations = await sql`
-    //     select * from locations
-    //     where title ilike ${pattern}
-    // `;
+    //   locations = await pg.execute(`
+    //     select * from locations where title like %${input}%
+    //   `);
     //   res.status(200).json(locations);
     // } catch (error) {
-    //   console.error("Failed to fetch your locations:", error);
+    //   console.error("Failed to fetch the location/s:", error);
     //   res.status(500).json({ error: "Internal Server Error" });
     // }
+
+    try {
+      const pattern = "%" + input + "%";
+      locations = await sql`
+        select * from locations
+        where title ilike ${pattern}
+    `;
+      res.status(200).json(locations);
+    } catch (error) {
+      console.error("Failed to fetch your locations:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
--- utils/pg.js
+++ utils/pg.js
@@ -1,15 +1,15 @@
-import { Client } from "pg";
-const pg = new Client({
-  connectionString: process.env.POSTGRES_CONNECTION_STRING,
-});
-pg.connect();
+// import { Client } from "pg";
+// const pg = new Client({
+//   connectionString: process.env.POSTGRES_CONNECTION_STRING,
+// });
+// pg.connect();
 
-export default {
-  ...pg,
-  execute: async (query, ...args) => {
-    const response = await pg.query(query, ...args);
-    console.log(response);
-    const rows = response.rows;
-    return rows;
-  },
-};
+// export default {
+//   ...,
+//   execute: async (query, ...args) => {
+//     const response = await pg.query(query, ...args);
+//     console.log(response);
+//     const rows = response.rows;
+//     return rows;
+//   },
+// };
