6d562712d1dc4eca3cae96412451afdceb3a0785
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-17 11:25:32 +0000

    Set the default value of buttons to empty array in the Footer component

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -1,11 +1,11 @@
 import FooterButton from "./footer-button";
 
-export default function Footer({ buttons }) {
+export default function Footer({ buttons = [] }) {
   return (
     <div className="w-full max-w-6xl mx-auto mt-8 flex flex-row space-x-2 px-4">
       {buttons.map((button, index) => (
         <FooterButton
-          key={button.index}
+          key={index}
           pageName={button.pageName}
           href={button.href}
         ></FooterButton>
