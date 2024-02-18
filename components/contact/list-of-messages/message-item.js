5ff8a84bea8ee3b6c56777f063c618848b267bf6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-18 08:06:18 +0000

    added svg icon to the delete button

--- components/contact/list-of-messages/message-item.js
+++ components/contact/list-of-messages/message-item.js
@@ -35,13 +35,31 @@ export default function MessageItem({ message, onDelete }) {
         <div className="w-full flex justify-center gap-x-2">
           <button
             disabled={!modalOpacity}
-            className="w-1/6 bg-red-500 text-white px-2 py-1 rounded"
+            className="w-1/5 bg-red-500 text-white px-2 py-1 rounded flex gap-2"
             onClick={() => onDelete(message.id)}
           >
-            Yes
+            <span>Yes</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-trash-2"
+            >
+              <path d="M3 6h18" />
+              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+              <line x1="10" x2="10" y1="11" y2="17" />
+              <line x1="14" x2="14" y1="11" y2="17" />
+            </svg>
           </button>
           <button
-            className="w-1/6 bg-blue-500 text-white px-2 py-1 rounded"
+            className="w-1/5 bg-blue-500 text-white px-2 py-1 rounded"
             onClick={() => setModalOpacity(false)}
           >
             No
