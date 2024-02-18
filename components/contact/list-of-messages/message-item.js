4e7b63bf320e808472b7c910639e05bda454a54e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-18 07:47:34 +0000

    added transition property to smooth out the opacity

--- components/contact/list-of-messages/message-item.js
+++ components/contact/list-of-messages/message-item.js
@@ -19,15 +19,15 @@ export default function MessageItem({ message, onDelete }) {
       className="bg-blue-100 py-4 px-8 rounded-lg shadow relative"
     >
       <span
-        className={`absolute right-0 top-0 translate-x-[60%] translate-y-[-60%] px-2 rounded-full cursor-pointer opacity-0 ${
+        className={`absolute z-50 right-0 top-0 translate-x-[60%] translate-y-[-60%] px-2 bg-red-400 text-white rounded-full cursor-pointer opacity-0 transition-opacity duration-500 ${
           isHovered ? "opacity-100" : "opacity-0"
-        } text-red-500 text-xl`}
+        } text-xl`}
         onClick={handleModal}
       >
         X
       </span>
       <div
-        className={`h-full w-full absolute top-0 left-0 bg-gray-50 flex flex-col justify-center items-center rounded-lg ${
+        className={`h-full w-full absolute top-0 left-0 bg-gray-50 flex flex-col justify-center items-center rounded-lg transition-opacity duration-500 ${
           modalOpacity ? "opacity-95" : "opacity-0"
         }`}
       >
